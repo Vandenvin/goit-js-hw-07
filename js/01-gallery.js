830b182a1a4f08b773b38e97257a6b6d9852e010
Serhii Vandenvin <sevandenvin@gmail.com>
2022-08-15 21:21:53 +0300

    task-01 almost complete
    need add cheking on Escape

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,64 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const imageMarkup = createGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeEnd', imageMarkup);
+galleryContainer.addEventListener('click', onImageClick);
+
+function createGallery(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      // <a class="gallery__link" href="${original}"> вставить вместо <a> ниже
+      // target="_blank - открытие ссылки в новом окне
+      // onclick = "return false" - не выполняется переход
+      return `
+        <a class="gallery__link" href="${original}" onclick = "return false">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+      `;
+    })
+    .join('');
+}
+
+function onImageClick(event) {
+  const isElementImage = event.target.classList.contains('gallery__image');
+  if (!isElementImage) {
+    return;
+  }
+
+  const img = event.target;
+  const urlToLargeImg = img.dataset.source;
+
+  const modal = `
+        <div class="modal">
+            <img src="${urlToLargeImg}">
+        </div>
+      `;
+  const createModal = basicLightbox.create(modal, {
+    onShow: () => {
+      window.addEventListener('keydown', onEscapePress);
+    },
+    onClose: () => {
+      window.removeEventListener('keydown', onEscapePress);
+    },
+  });
+
+  createModal.show();
+}
+
+function onEscapePress(event) {
+  console.log(event.code);
+  return event.code;
+}
+
+// event => {
+//   if (event.code === 'Escape') {
+//     createModal.close();
+//   }
+// };
