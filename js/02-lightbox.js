630486a7e8ad5109ec9ee9421ff8fa6f8da80b0f
Serhii Vandenvin <sevandenvin@gmail.com>
2022-08-16 17:23:36 +0300

    task-02 completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const imageMarkup = createGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeEnd', imageMarkup);
+
+function createGallery(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}" onclick = "return false">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      `;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
